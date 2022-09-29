e1c7d3539203afb4e7ecd821e3f1efca7fbecd5d
Lexi Grant <lexidgrant@gmail.com>
2022-09-28 22:53:44 -0500

    had to add crossorigin to not get an error

--- src/main/java/com/example/granttravelco_api/UserController.java
+++ src/main/java/com/example/granttravelco_api/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin()
 @RestController
 @RequestMapping("/api")
 
